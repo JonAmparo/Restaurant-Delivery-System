2d6567ecc8206b63304c090b4f97392cc1460b08
Jonathan Amparo <JonathanAmparo2@Live.com>
2019-01-21 17:28:53 -0500

    Update DTO.java
    Added Arraylist, singleton design and getters for the arrays

--- model/DTO.java
+++ model/DTO.java
@@ -2,7 +2,49 @@ package model;
 
 import java.util.ArrayList;
 
-public abstract class DTO {
-	static ArrayList<ClientBean> clientList = new ArrayList<>();
+/*
+ * DTO = Data Transfer Object
+*/
+public class DTO {
+	private ArrayList<ClientBean> clientList = new ArrayList<>();
+	private ArrayList<RestaurateurBean> restaurateurList = new ArrayList<>();
+	private ArrayList<LoginBean> userList = new ArrayList<>();
+
+	// create a variable to get the instance of this class
+	private static DTO current_instance = null;
+
+	// make the constructor private to prevent the instantiation of this class
+	private DTO() {
+	}
+
+	// create a function to get the instance of the class (getter)
+	public static DTO getInstance() {
+		if (current_instance == null)
+			current_instance = new DTO();
+		return current_instance;
+	}
+
+	/**
+	 * @return the clientList
+	 */
+	public ArrayList<ClientBean> getClientList() {
+		return clientList;
+	}
+
+	/**
+	 * @return the userList
+	 */
+	public ArrayList<LoginBean> getUserList() {
+		return userList;
+	}
+
+	/**
+	 * @return the restaurateurList
+	 */
+	public ArrayList<RestaurateurBean> getRestaurateurList() {
+		return restaurateurList;
+	}
+	
+	
 
 }
